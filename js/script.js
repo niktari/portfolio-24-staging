2612a382f40ac9e1392ce67990c6d390273fcd46
niktari <77365760+niktari@users.noreply.github.com>
2024-02-12 13:06:09 -0500

    update links

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 let infoButton = document.getElementById('nav--info');
 let infoElem = document.getElementById('info');
 let paramsNav = document.getElementById('nav--params')
+let topButton = document.getElementById('nav--top');
 
 function revealInfo() {
     infoElem.classList.toggle("grid");
@@ -10,10 +11,26 @@ function revealInfo() {
     if(infoElem.classList.contains("hide-element")) {
         infoButton.style.color = "var(--black-30)";
         paramsNav.style.display = 'flex';
+        topButton.style.display = 'flex';
     } else {
         infoButton.style.color = "var(--black)";
             paramsNav.style.display = 'none';
+            topButton.style.display = 'none'
     }
 }
 
 infoButton.addEventListener("click", revealInfo);
+
+// BACK TO TOP
+
+console.log(topButton)
+
+window.onscroll = function () {
+
+        if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30 && infoElem.classList.contains("hide-element")) {
+            topButton.style.display = "flex";
+        } else {
+            topButton.style.display = "none";
+        }
+
+}
